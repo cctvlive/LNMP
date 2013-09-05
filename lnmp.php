76fc91865e610cdee75a12654fb57a4fb71216cb
SH <cctv_243028755@live.cn>
2013-09-05 11:49:58 +0800

    2013/9/5
    cctv

--- lnmp.php
+++ lnmp.php
@@ -1,1 +1,18 @@
 操作日志:
+
+2013/9/5/1
+
+
+find / -name php.ini
+
+vi /www/..../php.ini
+
+/date.timezone
+
+date.timezone=PRC 
++c :wq ll 
+......................................... 
+
+pcretest -C @查询编码 : utf-8;
+
+find / -name nginx
